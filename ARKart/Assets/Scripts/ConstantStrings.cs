6039e2da37487fb747bb46e0125798fa2dfde372
Minja Silvennoinen <minja.silvennoinen@futurice.com>
2018-04-06 11:34:09 +0300

    Light and camera setup

---
+++ ARKart/Assets/Scripts/ConstantStrings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class ConstantStrings
+{
+	public const string DEFAULT = "Default";
+	public const string ANDROID = "Android";
+
+	// GameObject names
+	public const string CAMERAPARENT = "CameraParent";
+	public const string LIGHTCONTROLLER = "LightController";
+}
---
+++ ARKart/Assets/Scripts/GameObjectEnabler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectEnabler : MonoBehaviour 
+{
+	[Header("Cameras")]
+	[SerializeField]
+	private GameObject _cameraParent;
+
+	[SerializeField]
+	private List<GameObject> _sceneCameras = new List<GameObject>();
+
+	[Header("Lights")]
+	[SerializeField]
+	private GameObject _lightController;
+
+	[SerializeField]
+	private List<GameObject> _sceneLights = new List<GameObject>();
+
+	void Awake()
+	{
+		if(_cameraParent == null)
+			_cameraParent = GameObject.Find(ConstantStrings.CAMERAPARENT);
+		if(_lightController == null)
+			_lightController = GameObject.Find(ConstantStrings.LIGHTCONTROLLER);
+
+		InitializeLists();
+
+		#if UNITY_STANDALONE || UNITY_EDITOR
+		EnableObjects(ConstantStrings.DEFAULT);
+
+		#elif UNITY_ANDROID
+		EnableObjects(ConstantStrings.ANDROID);
+
+		#endif
+	}
+
+	///<summary>
+	/// Adds all cameras and lights to a list
+	///</summary>
+	void InitializeLists()
+	{
+		if(_sceneCameras.Count == 0 && _cameraParent != null)
+		{
+			for (int i = 0; i < _cameraParent.transform.childCount; i++)
+			{
+				_sceneCameras.Add(_cameraParent.transform.GetChild(i).gameObject);
+			}
+		}
+		else
+		{
+			Debug.LogWarning("References missing from GameController or it's children");
+		}
+
+		if(_sceneLights.Count == 0 && _lightController != null)
+		{
+			for (int i = 0; i < _lightController.transform.childCount; i++)
+			{
+				_sceneLights.Add(_lightController.transform.GetChild(i).gameObject);
+			}
+		}
+		else
+		{
+			Debug.LogWarning("References missing from GameController or it's children");
+		}
+	}
+
+	/// <summary>
+	/// Sets lights active depending on chosen platform
+	///</summary>
+	void EnableObjects(string deviceInUse) 
+	{
+		foreach (GameObject camera in _sceneLights)
+		{
+			Debug.Log("camera: " + camera);
+			bool isEnabled = (camera.layer == LayerMask.NameToLayer(deviceInUse)) ? true : false;
+			camera.SetActive(isEnabled);
+		}
+
+		foreach (GameObject light in _sceneLights)
+		{
+			bool isEnabled = (light.layer == LayerMask.NameToLayer(deviceInUse)) ? true : false;
+			light.SetActive(isEnabled);
+		}
+	}
+}
